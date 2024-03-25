69114b3410b71762e051cbbd173e527f67711520
Berlandro <bryangodlive0@gmail.com>
2024-03-25 07:56:11 +0700

    initial commit

---
+++ src/learn/java/pkg2/LearnJava2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package learn.java.pkg2;
+
+/**
+ *
+ * @author X1 PRO
+ */
+public class LearnJava2 {
+
+    public static void main(String[] args) {
+        boolean a,b,c;
+        
+        System.out.println("---- OR ----");
+        a = true;
+        b = false;
+        c = (a ^ b);
+        System.out.println("A dengan B = " + c);
+    }
+    
+}
